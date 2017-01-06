9cc7ecb4d560b6cebc79f363ccb2de428c1c6223
todus25 <almostfemous@gmail.com>
2017-01-06 16:05:52 +0600

    hi

---
+++ index.php
@@ -0,0 +1,76 @@
+<style>
+h1{color:green;
+    text-align:center;
+	font-size:100px;
+    };
+</style>
+<h1>
+<?php
+//$x=1.5;
+//$y=2;
+
+//echo "<h1>hello world</h1>";
+//echo $x*$y.'hi';
+//$x=array('book','pen');
+//echo var_dump($x);
+
+/*$x=1;
+$y=2;
+$x+=2;
+echo $x;*/
+
+
+/*$x=2;
+if($x==1):
+	echo'true';
+	else:
+	echo'false';
+	endif;*/
+/*	
+$x=2;
+$y=2;
+$z=5;
+if(($x>=1 && $y<=2)&& $z>5):
+
+	echo 'true';
+	
+	else:
+	
+	echo'false';
+	
+	endif;*/
+	
+/*for($x=1;$x<=10;$x++)
+{
+	echo$x.'<br>';
+	if($x==5){break;}
+}*///for($x=1;$x<=10;$x++)
+/*{  $i+=$x;
+	echo$x.'<br>';
+
+}*/
+/*$i=0;
+while($i<=10){
+   echo $i.'<br>';
+   $i+=2;
+}	*/
+
+/*$i=array('b','p','h');
+$x=1;
+foreach($i as $p){
+	echo $x.$p.'<br>';
+	$x+=2;
+}*/
+function gulti($x,$y){
+	         
+			      
+				 
+				 return $x.$y.'!!!';
+			 
+		        	    
+}
+
+echo gulti(1,2);
+
+?> 
+</h1>
