158c3039abbe256cafdc6ca2ac6a7156d4fef36e
todus25 <almostfemous@gmail.com>
2017-01-07 17:52:57 +0600

    abar

--- new4.php
+++ new4.php
@@ -2,14 +2,22 @@
 
 $y=array(
 
-      //  array('name'=>'al imran', 'pen'=>'10', 'book'=>'200'),
-       // array('name'=>'habib', 'pen'=>'12', 'book'=>'400')
+        array('name'=>'al imran', 'pen'=>'10', 'book'=>'200'),
+        array('name'=>'habib', 'pen'=>'12', 'book'=>'400')
 	   
-	   array('al imran','10','200'),
-        array('habib','12','400')
+	  // array('al imran','10','200'),
+        //array('habib','12','400')
 	   
 	   
 		);
 		echo $y[1][2];
 		
-		var_dump ( $_SERVER);
+		//var_dump ($_SERVER);
+		foreach($y as $k=>$v)
+		{
+			
+			echo$y[$k]['name'].'</br>';
+		}
+			
+			
+		
